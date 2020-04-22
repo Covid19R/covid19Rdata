9006fd732467d1f3438d8597c4fad0de43f9f852
Jarrett Byrnes <jebyrnes@Jarretts-MacBook-Air-2.local>
2020-04-22 14:21:22 -0400

    Updating with debusklaneml/CanadaC19

--- R/cron_job_dependencies.R
+++ R/cron_job_dependencies.R
@@ -1,3 +1,4 @@
 # Required covid19 packages
 devtools::install_github("Covid19R/covid19france")
 devtools::install_github("Covid19R/covid19nytimes")
+devtools::install_github("debusklaneml/CanadaC19")
