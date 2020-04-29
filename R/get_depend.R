f8f634bd9b443fabb65396720de9fb0a10ee7e71
RamiKrispin <ramkrisp@umich.edu>
2020-04-29 15:53:47 -0700

    update the cron jab function

---
+++ R/get_depend.R
@@ -0,0 +1,20 @@
+# Required covid19 packages
+get_depend <- function(verbose = TRUE){
+  if(verbose) {
+    message("Installing the supporting packages")
+  }
+  args <-
+    readr::read_csv("https://raw.githubusercontent.com/Covid19R/covid19Rdata/rami-dev/data-raw/packages.csv", col_types = "cc") %>%
+    dplyr::mutate(
+      arg = glue::glue("{username}/{package}")
+    ) %>%
+    dplyr::pull(arg)
+  
+  for (a in args) {
+    devtools::install_github(a)
+    if(verbose){
+      message(paste(a, "was installed", sep = " "))
+    }
+  }
+ return(message("Done.."))  
+}
