f25d2d3fec52beddb91fb5657e9b139aed244f2a
RamiKrispin <ramkrisp@umich.edu>
2020-05-03 00:58:56 -0700

    update the file path

--- R/acquire_data.R
+++ R/acquire_data.R
@@ -114,7 +114,7 @@ acquire_data <- function(verbose = TRUE) {
   
   # Add old info for failed packages ####
   # Load the past table of datasets and info from previous get_info
-  past_data_info <- readr::read_csv("./data-raw/covid19R_data_info.csv")
+  past_data_info <- read.csv("data-raw/covid19R_data_info.csv")
   
   if (sum(errors_in_getinfo) > 0) {
     bad_pkg <- names(errors_in_getinfo)
