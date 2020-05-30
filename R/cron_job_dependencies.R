638fef0ca72917b1daa2b4ed80b03adb46f6dee4
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2020-05-30 13:11:38 -0400

    dependency install fix

--- R/cron_job_dependencies.R
+++ R/cron_job_dependencies.R
@@ -4,19 +4,17 @@
 #' 
 
 install_depend <- function(verbose = TRUE){
-  
   if(verbose) {
     message("Installing the supporting packages")
   }
   args <-
-    readr::read_csv("https://raw.githubusercontent.com/Covid19R/covid19Rdata/rami-dev/data-raw/packages.csv", col_types = "cc") %>%
+    readr::read_csv("https://raw.githubusercontent.com/Covid19R/covid19Rdata/master/data-raw/packages.csv", col_types = "cc") %>%
     dplyr::mutate(
       arg = glue::glue("{username}/{package}")
     ) %>%
     dplyr::pull(arg)
-  
   for (a in args) {
-    devtools::install_github(a)
+    devtools::install_github(a, upgrade = "never")
     if(verbose){
       message(paste(a, "was installed", sep = " "))
     }
