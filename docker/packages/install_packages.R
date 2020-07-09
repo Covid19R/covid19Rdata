04cd764677b0118523677580848528e5a056635b
RamiKrispin <ramkrisp@umich.edu>
2020-07-09 05:59:17 -0700

    add error handling

--- docker/packages/install_packages.R
+++ docker/packages/install_packages.R
@@ -1,5 +1,6 @@
 # installing package imports packages
 pkg_list <- c("dplyr",
+              "tibble",
               "devtools",
               "fs",
               "gh",
@@ -15,3 +16,10 @@ pkg_list <- c("dplyr",
               "usethis")
 
 install.packages(pkgs = pkg_list, repos = "https://cran.rstudio.com/")
+
+for(i in pkg_list){
+  
+  if(!i %in% rownames(installed.packages())){
+    stop(paste("Package", i, "is not available"))
+  }
+}
