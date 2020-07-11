cf934da1abc480506e0681fd5c8605affdfebe90
RamiKrispin <ramkrisp@umich.edu>
2020-07-11 14:10:14 -0700

    replacing functions from the glue package with the base package

--- R/get_package_info.R
+++ R/get_package_info.R
@@ -1,13 +1,11 @@
 get_package_info <- function(pkg, verbose = TRUE) {
 
   if (verbose) {
-    message(
-      glue::glue("Getting info for {pkg}\n")
-    )
+    cat("Getting info for", pkg, "\n")
   }
   
   # make the call for the package
-  run <- glue::glue("{pkg}::get_info_{pkg}()")
+  run <- sprintf("%s::get_info_%s()", pkg, pkg)
 
   try(eval(parse(text = run)))
 }
