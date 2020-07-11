a3b817ace6f68a2d35ecda681d26d53e32043515
RamiKrispin <ramkrisp@umich.edu>
2020-07-10 20:34:08 -0700

    repleaching the read_csv function

--- R/get_depend.R
+++ R/get_depend.R
@@ -6,18 +6,16 @@ get_depend <- function(verbose = TRUE){
   if(verbose) {
     message("Installing the supporting packages")
   }
-  args <-
-    readr::read_csv("https://raw.githubusercontent.com/Covid19R/covid19Rdata/rami-dev/data-raw/packages.csv", col_types = "cc") %>%
-    dplyr::mutate(
-      arg = glue::glue("{username}/{package}")
-    ) %>%
-    dplyr::pull(arg)
+  
+  packages_list <- utils::read.csv("https://raw.githubusercontent.com/Covid19R/covid19Rdata/master/data-raw/packages.csv", 
+                                   stringsAsFactors = FALSE) 
+  args <-paste(packages_list$username, packages_list$package, sep = "/")
   
   for (a in args) {
-    devtools::install_github(a)
+    devtools::install_github(a, upgrade = "never")
     if(verbose){
       message(paste(a, "was installed", sep = " "))
     }
   }
- return(message("Done.."))  
+  return(message("Done.."))  
 }
