85d60c0b4c204cb640dbd534a0b319c03111593d
RamiKrispin <ramkrisp@umich.edu>
2020-07-10 20:46:12 -0700

    update the docker

--- docker/packages/install_packages.R
+++ docker/packages/install_packages.R
@@ -27,3 +27,15 @@ for(i in pkg_list){
     stop(paste("Package", i, "is not available"))
   }
 }
+
+# install Covid19R project packages
+packages_list <- utils::read.csv("https://raw.githubusercontent.com/Covid19R/covid19Rdata/master/data-raw/packages.csv", 
+                                 stringsAsFactors = FALSE) 
+args <-paste(packages_list$username, packages_list$package, sep = "/")
+
+for (a in args) {
+  devtools::install_github(a, upgrade = "never")
+  if(verbose){
+    message(paste(a, "was installed", sep = " "))
+  }
+}
